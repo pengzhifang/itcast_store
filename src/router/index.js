bad126644ed5e7217161eddef3c1964a3329234e
peng zhifang <1318255539@qq.com>
2018-07-08 15:39:46 +0800

    Home页面结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '../views/Login.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       name: 'login',
       path: '/login',
       component: Login
+    },
+    {
+      name: 'home',
+      path:  '/',
+      component: Home
     }
   ]
 });
