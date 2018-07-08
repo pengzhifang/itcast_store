bb8130be99e2da1cc4e1081d9d763be2cc3002ac
peng zhifang <1318255539@qq.com>
2018-07-08 11:40:17 +0800

    登录-配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Login from '../views/Login.vue';
 
-Vue.use(Router)
-
-;(function fn () {
-
-})();
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      helsdfjs
+   </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
