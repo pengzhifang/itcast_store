f2fb8bbde6d8a16e2568cbdf46e398e7b32f7ca4
peng zhifang <1318255539@qq.com>
2018-07-09 00:19:21 +0800

    users表格

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -13,12 +13,89 @@
         <el-button type="success" plain>添加用户</el-button>
       </el-col>
     </el-row>
+    <el-table
+      stripe
+      border
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+      type="index"
+      width="50">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        label="创建日期">
+        <template slot-scope="scope">
+          {{ scope.row.create_time }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="用户状态"
+        width="130">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column
+      label="操作">
+        <template slot-scope="scope">
+          <el-button plain size="mini" type="primary" icon="el-icon-edit"></el-button>
+          <el-button plain size="mini" type="danger" icon="el-icon-delete"></el-button>
+          <el-button plain size="mini" type="warning" icon="el-icon-check"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      // 发送请求之前，获取token
+      const token = sessionStorage.getItem('token');
+      // 在请求头中设置token
+      this.$http.defaults.headers.common['Authorization'] = token;
+      const res = await this.$http.get('/users?pagenum=1&pagesize=10');
+      console.log(res);
+      const data = res.data;
+      const {meta: {msg, status}} = data;
+      if (status === 200) {
+        const {data: {users}} = data;
+        if (status === 200) {
+          this.list = users;
+        } else {
+          this.$message.error(msg);
+        }
+      }
+    }
+  }
 };
 </script>
 
