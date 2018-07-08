f0a61257cabd7a6f1bf1b12995b6cc3fb1e5aea6
peng zhifang <1318255539@qq.com>
2018-07-08 21:20:14 +0800

    面包屑和搜索区域

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <el-card class="box-card">
-      kapianneirong 
-    </el-card>
-  </div>
+  <el-card class="box-card">
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="bread">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row class="searchArea">
+      <el-col :span="24">
+        <el-input class="searchInput" clearable placeholder="请输入内容">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
@@ -13,5 +23,13 @@ export default {
 </script>
 
 <style>
-
+.box-card {
+  height: 100%;
+}
+.searchArea {
+  margin: 15px 0;
+}
+.searchArea .searchInput{
+  width: 350px;
+}
 </style>
