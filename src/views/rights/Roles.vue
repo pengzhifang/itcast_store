f1634a447ec1f114f0a81185d7897b37c777916e
peng zhifang <1318255539@qq.com>
2018-07-11 14:25:19 +0800

    展示所有权限

--- src/views/rights/Roles.vue
+++ src/views/rights/Roles.vue
@@ -16,8 +16,43 @@
       style="width: 100%">
       <!-- 展开列 -->
       <el-table-column type="expand">
-        <template>
-          <el-tag type="success" closable @close="handleClose">哈哈</el-tag>
+        <template slot-scope="scope">
+          <el-row
+          v-for="item1 in scope.row.children"
+          :key="item1.id"
+          class="level1">
+            <!-- 一级权限 -->
+            <el-col :span="4">
+              <el-tag closable>{{ item1.authName }}</el-tag>
+            </el-col>
+            <el-col :span="20">
+               <!-- 二级权限 -->
+              <el-row
+              v-for="item2 in item1.children"
+              :key="item2.id">
+                <el-col :span="4">
+                  <el-tag closable type="success">{{ item2.authName }}</el-tag>
+                </el-col>
+                 <!-- 三级权限 -->
+                <el-col :span="20">
+                  <el-tag
+                    v-for="item3 in item2.children"
+                    :key="item3.id"
+                    closable
+                    type="warning"
+                    class="level3">
+                    {{ item2.authName }}
+                  </el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <!-- 未分配角色 -->
+          <el-row v-if="scope.row.children.length === 0">
+            <el-col :span="24">
+              未分配角色
+            </el-col>
+          </el-row>
         </template>
       </el-table-column>
       <el-table-column
@@ -71,7 +106,7 @@ export default {
     },
     // 移除标签事件
     handleClose() {
-
+      // alert(1);
     }
   }
 };
@@ -80,5 +115,11 @@ export default {
 <style>
 .btn {
   margin: 15px 0;
+}
+.level1 {
+  margin-bottom: 10px;
+}
+.level3 {
+  margin: 0 5px 5px 0;
 }
 </style>
