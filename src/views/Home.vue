f78a55df04e23db588410157f7832514e14df5a4
peng zhifang <1318255539@qq.com>
2018-07-11 20:58:35 +0800

    动态展示左侧菜单权限

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,66 +19,20 @@
           unique-opened
           router
           class="el-menu-vertical-demo">
-          <el-submenu index="1">
+          <el-submenu
+          :index="item1.id + ''"
+          v-for="item1 in menus"
+          :key="item1.id">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{ item1.authName }}</span>
             </template>
-            <el-menu-item index="/users">
+            <el-menu-item
+            :index="'/' + item2.path"
+            v-for="item2 in item1.children"
+            :key="item2.id">
               <i class="el-icon-menu"></i>
-              用户列表
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="/roles">
-              <i class="el-icon-menu"></i>
-              角色列表
-            </el-menu-item>
-            <el-menu-item index="/rights">
-              <i class="el-icon-menu"></i>
-              权限列表
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item index="3-1">
-              <i class="el-icon-menu"></i>
-              商品列表
-            </el-menu-item>
-            <el-menu-item index="3-2">
-              <i class="el-icon-menu"></i>
-              分类参数
-            </el-menu-item>
-            <el-menu-item index="3-3">
-              <i class="el-icon-menu"></i>
-              商品分类
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item index="4-1">
-              <i class="el-icon-menu"></i>
-              订单列表
-            </el-menu-item>
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item index="5-1">
-              <i class="el-icon-menu"></i>
-              数据报表
+              {{ item2.authName }}
             </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -92,6 +46,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      menus: []
+    };
+  },
   beforeCreate() {
     const token = sessionStorage.getItem('token');
     if (!token) {
@@ -99,7 +58,20 @@ export default {
       this.$message.warning('请先登录!');
     }
   },
+  created() {
+    this.loadData();
+  },
   methods: {
+    async loadData() {
+      const res = await this.$http.get('menus');
+      console.log(res);
+      const {meta: {msg, status}, data} = res.data;
+      if (status === 200) {
+        this.menus = data;
+      } else {
+        this.$message.error(msg);
+      }
+    },
     handleLogout() {
       sessionStorage.clear();
       this.$router.push({name: 'login'});
