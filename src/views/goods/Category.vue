8221e37395a0d187d267171a3f41cb6fd61c8ad0
peng zhifang <1318255539@qq.com>
2018-07-12 10:49:54 +0800

    商品分类-展示列表数据

--- src/views/goods/Category.vue
+++ src/views/goods/Category.vue
@@ -1,15 +1,71 @@
 <template>
-  <div>
-    商品分类
-  </div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <my-breadcrumb level1="商品管理" level2="商品分类"></my-breadcrumb>
+    <!-- 添加分类按钮 -->
+    <el-row class="btn">
+      <el-col :span="24">
+        <el-button type="success" plain>添加分类</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <el-table
+    border
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        prop="cat_name"
+        label="分类名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        label="级别"
+        width="300">
+        <template slot-scope="scope">
+          <span v-if="scope.row.cat_level === 0">一级</span>
+          <span v-else-if="scope.row.cat_level === 1">二级</span>
+          <span v-else-if="scope.row.cat_level === 2">三级</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="是否有效">
+        <template slot-scope="scope">
+           {{ scope.row.cat_deleted ? '无效': '有效'}}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button plain size="mini" type="primary" icon="el-icon-edit"></el-button>
+          <el-button plain size="mini" type="danger" icon="el-icon-delete"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const res = await this.$http.get('categories?type=3');
+      console.log(res);
+      const { data } = res.data;
+      this.list = data; 
+    }
+  }
 };
 </script>
 
 <style>
-
+.btn {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
